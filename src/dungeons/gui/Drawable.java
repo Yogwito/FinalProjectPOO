637c0439565b78af7a10992f2ff2aec750f43668
Sebastian Arias <juansearias21@gmail.com>
2025-05-24 11:51:18 -0500

    gui drawable creada

---
+++ src/dungeons/gui/Drawable.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dungeons.gui;
+
+
+/**
+ * Esta interfaz define un objeto que puede ser dibujado en una interfaz gráfica.
+ * Los objetos que implementan esta interfaz deben proporcionar una implementación para el método 'redraw()'.
+ * @author Santiago Jiménez
+ * @author Daniel Felipe Lopez
+ * @version 1.0.2
+ */
+public interface Drawable {
+
+    /**
+     * Este método se utiliza para redibujar el objeto.
+     */
+    public void redraw();
+}
