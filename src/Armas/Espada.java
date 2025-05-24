380d890f3df7114c8e5a635fe872f3382314e546
Juan Jose Cardona Daza <Jcardonadaza@gmail.com>
2025-05-24 11:37:46 -0500

    se agrega la clase Espada

---
+++ src/Armas/Espada.java
@@ -0,0 +1,63 @@
+ode
+Espada.java (2 KB)
+file_download Download JAVA Save to Drive Save to Drive more_vert
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Armas;
+
+import Class.Dungeon;
+import Class.LivingBeing;
+import Class.Sprite;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
+/**
+ * Esta es una clase Espada que hereda de la clase Weapon.
+ * Representa un arma específica (Espada) en el juego.
+ *
+ * @author Juan Jose Cardona Daza
+ * @author Juan Jose Trujillo
+ * @author Juan Sebastian Arias
+ * @version 1.0.2
+ */
+public class Espada extends Weapon{
+    
+    /**
+     * Constructor de la clase Espada.
+     * Inicializa las propiedades de la clase con los valores proporcionados.
+     *
+     * @param x        La coordenada x del arma.
+     * @param y        La coordenada y del arma.
+     * @param width    El ancho del arma.
+     * @param height   La altura del arma.
+     * @param dungeon  El calabozo en el que se encuentra el arma.
+     * @param path     La ruta del archivo de imagen para el arma.
+     */
+    public Espada(int x, int y, int width, int height, Dungeon dungeon, String path) {
+        super(x, y, width, height, Color.black, dungeon, path);
+    }
+    
+    /**
+     * Comprueba si esta arma colisiona con otro sprite.
+     *
+     * @param sprite El otro sprite con el que se está comprobando la colisión.
+     * @return true si existe colisión, false en caso contrario.
+     */
+    @Override
+    public boolean checkCollision(Sprite sprite) {
+        return super.checkCollision(sprite);
+    }
+
+    /**
+     * Método para redibujar el arma.
+     * Este método aún no está implementado.
+     */
+    @Override
+    public void redraw() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
